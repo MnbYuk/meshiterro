8d6250dd14c0259e9ebdd0bcea413597b7ef668a
MnbYuk <haiiroorennji@gmail.com>
2024-05-05 03:54:25 +0000

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
@@ -11,7 +17,6 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
